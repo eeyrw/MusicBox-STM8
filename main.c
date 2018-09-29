de35b5a2b3b658a932a4b520044567ef37cec5bc
yuan <yrw@sdec.com.cn>
2018-09-29 15:53:33 +0800

    Update some code.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define OUTPUT_PIN 3
 
 extern uint16_t PitchIncrementTable[];
-extern uint8_t EnvelopeTable[];
+extern uint8_t EnvelopeTable[256];
 extern int8_t WaveTable[];
 typedef struct _SoundUnit
 {
@@ -21,8 +21,8 @@ uint16_t globalTimer = 0;
 
 SoundUnit Sounds[POLY_NUM];
 
-#define WAVETABLE_ATTACK_LEN 1000
-#define WAVETABLE_LEN 1200
+#define WAVETABLE_ATTACK_LEN 1630
+#define WAVETABLE_LEN 1758
 #define WAVETABLE_LOOP_LEN (WAVETABLE_LEN - WAVETABLE_ATTACK_LEN)
 
 uint8_t playstatus=0;
@@ -44,12 +44,19 @@ void NoteOn(uint8_t note)
 
 void GenDecayEnvlope()
 {
-	for (uint8_t i = 0; i < POLY_NUM; i++)
+	static uint8_t TimeCnt=0;
+	if(TimeCnt!=255)
+		TimeCnt++;
+	else
 	{
-		if (Sounds[i].wavetablePos >= WAVETABLE_ATTACK_LEN &&
-			Sounds[i].wavetablePos < sizeof(EnvelopeTable) - 1)
+		TimeCnt=0;
+		for (uint8_t i = 0; i < POLY_NUM; i++)
 		{
-			Sounds[i].envelopePos += 1;
+			if (Sounds[i].wavetablePos >= WAVETABLE_ATTACK_LEN &&
+				Sounds[i].wavetablePos < sizeof(EnvelopeTable) - 1)
+			{
+				Sounds[i].envelopePos += 1;
+			}
 		}
 	}
 }
@@ -89,8 +96,8 @@ void main()
 	PD_DDR |= (1 << OUTPUT_PIN);
 	PD_CR1 |= (1 << OUTPUT_PIN);
 
-	/* Prescaler = 128 */
-	TIM4_PSCR = 0b00000111;
+	/* Prescaler = 4 */
+	TIM4_PSCR = 0b00000010;
 
 	/* Frequency = F_CLK / (2 * prescaler * (1 + ARR))
      *           = 16 MHz / (2 * 4 * (1 + 124)) = 32000 Hz */
