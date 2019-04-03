6866586f182f60fdbe5463f505f16bc582e50d3c
yuan <yrw@sdec.com.cn>
2019-04-03 15:37:24 +0800

    Modifying.

--- AlgorithmTest.c
+++ AlgorithmTest.c
@@ -14,10 +14,59 @@ void TestInit(void)
     SynthInit(&synthesizerASM);
 }
 
+void PrintParameters(Synthesizer* synth)
+{
+    SoundUnitUnion* soundUnionList;
+    printf("MixOut:",synth->mixOut);
+    soundUnionList=&(synth->SoundUnitUnionList[0]);
+
+    printf("%12s","Chn Val");
+    for(uint8_t k=0;k<POLY_NUM;k++)
+    {
+        printf("%6d ",soundUnionList[k].combine.val);
+    }
+    printf("\n");
+
+    printf("%12s","Chn Sample");
+    for(uint8_t k=0;k<POLY_NUM;k++)
+    {
+        printf("%6d ",soundUnionList[k].combine.sampleVal);
+    }
+    printf("\n");
+
+    printf("%12s","Chn EnvlLevel");
+    for(uint8_t k=0;k<POLY_NUM;k++)
+    {
+        printf("%6d ",soundUnionList[k].combine.envelopeLevel);
+    }
+    printf("\n");
+
+
+    printf("%12s","Chn WavePos");
+    for(uint8_t k=0;k<POLY_NUM;k++)
+    {
+        printf("%6d ",soundUnionList[k].combine.wavetablePos_int);
+    }
+    printf("\n");
+
+    printf("%12s","Chn EnvlPos");
+    for(uint8_t k=0;k<POLY_NUM;k++)
+    {
+        printf("%6d ",soundUnionList[k].combine.envelopePos);
+    }
+    printf("\n");
+
+    printf("%12s","Chn NoteIncr");
+    for(uint8_t k=0;k<POLY_NUM;k++)
+    {
+        printf("%6d ",soundUnionList[k].combine.increment);
+    }
+    printf("\n");
+       
+}
 
 void TestProcess(void)
 {
-    SoundUnitUnion* soundUnionList;
     TestInit();
     for(uint8_t i=0;i<POLY_NUM;i++)
     {
@@ -26,47 +75,14 @@ void TestProcess(void)
     }
     for(uint8_t i=0;i<TEST_LOOP_NUN;i++)
     {
-
         Synth(&synthesizerASM);
         SynthC(&synthesizerC);
         GenDecayEnvlope(&synthesizerC);
         GenDecayEnvlopeAsm(&synthesizerASM);
-
         printf("=============%d==============\n",i);
-        printf("Synth C: MixOut: %6d EachChn: ",synthesizerC.mixOut);
-        soundUnionList=&(synthesizerC.SoundUnitUnionList[0]);
-        for(uint8_t k=0;k<POLY_NUM;k++)
-        {
-            printf("%6d ",soundUnionList[k].combine.val);
-        }
-        printf("\n                                 ");
-        for(uint8_t k=0;k<POLY_NUM;k++)
-        {
-            printf("%6d ",soundUnionList[k].combine.sampleVal);
-        }
-        printf("\n                               WP");
-        for(uint8_t k=0;k<POLY_NUM;k++)
-        {
-            printf("%6u",soundUnionList[k].combine.wavetablePos_int);
-        }
-        printf("\n");
-
-        printf("Synth A: MixOut: %6d EachChn: ",synthesizerASM.mixOut);
-        soundUnionList=&(synthesizerASM.SoundUnitUnionList[0]);
-        for(uint8_t k=0;k<POLY_NUM;k++)
-        {
-            printf("%6d ",soundUnionList[k].combine.val);
-        }
-        printf("\n                                 ");
-        for(uint8_t k=0;k<POLY_NUM;k++)
-        {
-            printf("%6d ",soundUnionList[k].combine.sampleVal);
-        }
-        printf("\n                               WP");
-        for(uint8_t k=0;k<POLY_NUM;k++)
-        {
-            printf("%6u",soundUnionList[k].combine.wavetablePos_int);
-        }
-        printf("\n");
+        printf("Synth C:\n");
+        PrintParameters(&synthesizerC);
+        printf("Synth ASM:\n");
+        PrintParameters(&synthesizerASM);
     }
 }
