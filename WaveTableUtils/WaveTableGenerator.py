b15386cafa24296647434cde5875c4cd384c2f39
yuan <yrw@sdec.com.cn>
2019-04-09 18:53:52 +0800

    Update wavetable generator.
    # Conflicts:
    #	WaveTableUtils/WaveTableGenerator.py

--- WaveTableUtils/WaveTableGenerator.py
+++ WaveTableUtils/WaveTableGenerator.py
@@ -31,7 +31,7 @@ def readWaveSamples(file_path):
 def calcIncrement(baseFreq,targetFreq):
     return targetFreq/baseFreq   
 
-def exportToSourceFile(sampleName,attackSamples,loopSamples,fileDir,sampleWidth):
+def exportToSourceFile(sampleName,attackSamples,loopSamples,freq,fileDir,sampleWidth):
     waveTableIdentifer="WaveTable_%s"%sampleName
     attackLen=len(attackSamples)
     loopLen=len(loopSamples)
@@ -49,6 +49,8 @@ def exportToSourceFile(sampleName,attackSamples,loopSamples,fileDir,sampleWidth)
     with open(hFilePath,'w') as hFile:
         hFile.write("#ifndef __%s__\n#define __%s__\n"%(str.upper(waveTableIdentifer),str.upper(waveTableIdentifer)))
         hFile.write("#include <stdint.h>\n")
+        hFile.write("// Sample's base frequency: %f Hz\n"%freq)
+        hFile.write("// Sample's sample rate: %d Hz\n"%32000)
         hFile.write("#define %s_LEN %d\n"%(str.upper(waveTableIdentifer),totalLen))
         hFile.write("#define %s_ATTACK_LEN %d\n"%(str.upper(waveTableIdentifer),attackLen))
         hFile.write("#define %s_LOOP_LEN %d\n"%(str.upper(waveTableIdentifer),loopLen))
@@ -58,11 +60,13 @@ def exportToSourceFile(sampleName,attackSamples,loopSamples,fileDir,sampleWidth)
 
     with open(cFilePath,'w') as cFile:
         cFile.write("#include <stdint.h>\n#include <%s.h>\n"%waveTableIdentifer)
+        cFile.write("// Sample's base frequency: %f Hz\n"%freq)
+        cFile.write("// Sample's sample rate: %d Hz\n"%32000)
         cFile.write("const %s %s[%s_LEN]={\n"%(sampleType,waveTableIdentifer,str.upper(waveTableIdentifer)))
         newLineCounter=0
         cFile.write("// Attack Samples:\n")
         for sample in attackSamples:
-            cFile.write("%8d,"%sample)
+            cFile.write("%6d,"%sample)
             if newLineCounter>8:
                 newLineCounter=0
                 cFile.write("\n")
@@ -70,17 +74,17 @@ def exportToSourceFile(sampleName,attackSamples,loopSamples,fileDir,sampleWidth)
                 newLineCounter+=1
         cFile.write("\n// Loop Samples:\n")
         for sample in loopSamples:
-            cFile.write("%8d,"%sample)
+            cFile.write("%6d,"%sample)
             if newLineCounter>8:
                 newLineCounter=0
                 cFile.write("\n")
             else:
                 newLineCounter+=1        
-        cFile.write("};\n")
+        cFile.write("};\n\n")
         cFile.write("const uint16_t %s_Increment[]={\n"%waveTableIdentifer)
         newLineCounter=0
         for i in range(57,120):
-            cFile.write("%5d,"%(calcIncrement(523.25,noteToFreq(i))*255))
+            cFile.write("%6d,"%(calcIncrement(freq,noteToFreq(i))*255))
             if newLineCounter>8:
                 newLineCounter=0
                 cFile.write("\n")
@@ -91,9 +95,11 @@ def exportToSourceFile(sampleName,attackSamples,loopSamples,fileDir,sampleWidth)
 
 
 def main():
-    attackSamples=readWaveSamples("./WaveTableUtils/Celesta_C5_ATTACK.wav")	
-    loopSamples=readWaveSamples("./WaveTableUtils/Celesta_C5_LOOP.wav")
-    exportToSourceFile("Celesta_C5",attackSamples,loopSamples,'./WaveTableUtils/',1)
+    sampleName="Celesta_C6"
+    sampleFreq=1046.502
+    attackSamples=readWaveSamples("./WaveTableUtils/%s_ATTACK.wav"%sampleName)	
+    loopSamples=readWaveSamples("./WaveTableUtils/%s_LOOP.wav"%sampleName)
+    exportToSourceFile(sampleName,attackSamples,loopSamples,sampleFreq,'./WaveTableUtils/',1)
     
 if __name__ == "__main__":
 	main()
